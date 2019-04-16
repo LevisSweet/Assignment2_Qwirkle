e7d0ed8626b74324cb5dece0f565a3ba0bc1aff0
LevisSweet <38487306+LevisSweet@users.noreply.github.com>
2019-04-16 21:02:24 +1000

    add file main for testing

---
+++ Module 1/include/Lesson1.h
@@ -0,0 +1,17 @@
+#ifndef LESSON1_H
+#define LESSON1_H
+
+
+class Lesson1
+{
+    public:
+        Lesson1();
+        virtual ~Lesson1();
+        calc();
+
+    protected:
+
+    private:
+};
+
+#endif // LESSON1_H
